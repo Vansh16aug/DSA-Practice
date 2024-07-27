4ee26c961ea7bffc22d04e482c0bed967fd27fa8
Vansh16aug <kumarvansh16aug@gmail.com>
2024-07-27 23:50:30 +0530

    feat: Improve function to count characters different from neighbors

--- Strings/1-Neighbouring-char-diff.cpp
+++ Strings/1-Neighbouring-char-diff.cpp
@@ -1,24 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-int func(string str){
-    int count=0;
-    for(int i=1;i<str.length()-1;i++){
-        if(str[i]!=str[i-1] && str[i]!=str[i+1]){
+
+int func(string str) {
+    int n = str.length();
+    if (n < 2) return n; // For strings with less than 2 characters
+
+    int count = 2; // Starting count for the first and last character
+
+    for (int i = 1; i < n - 1; i++) {
+        if (str[i] != str[i - 1] && str[i] != str[i + 1]) {
             count++;
-        }
-        else{
-            continue;
         }
     }
     return count;
 }
-int main()
-{
+
+int main() {
     string str;
-    getline(cin,str);
+    getline(cin, str);
     int ans = func(str);
-    cout<<ans<<endl;
+    cout << ans << endl;
     return 0;
 }
-
-// abbcdeffghh
